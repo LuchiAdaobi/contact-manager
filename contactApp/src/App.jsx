fe50a774f60c507a0c9a5bc4b5c90145af608f41
Luchi <luchichronicles@gmail.com>
2023-06-20 20:10:18 -0700

    initial commit

---
+++ contactApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+// import AddContact from './components/AddContact'
+// import ContactList from './components/ContactList'
+
+function App() {
+
+  return (
+    <>
+     <Header/> 
+     {/* <AddContact/>  */}
+     {/* <ContactList/>  */}
+    </>
+  )
+}
+
+export default App
